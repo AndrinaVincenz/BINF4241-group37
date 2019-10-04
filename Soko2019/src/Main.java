cf53e07ca868726b0f3a1ead79b56c8ba93e4dc0
Raphael <Koch@dhcp-wlan-uzh-10-12-152-124.uzh.ch>
2019-10-04 17:54:58 +0200

    read size of Squares

--- Soko2019/src/Main.java
+++ Soko2019/src/Main.java
@@ -1,9 +1,18 @@
+import java.util.Scanner;
 
 public class Main {
 
-	public static void main(String[] args){
 	
+	public static void main(String[] args){
 		
+		Scanner in = new Scanner(System.in);
+
+		int board;
+		System.out.println("How is the size of the board?");
+		board = in.nextInt();
+		System.out.println(board);
+	
+	
 		
 	}
 }
