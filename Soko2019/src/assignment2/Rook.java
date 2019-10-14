b7bcf242dd72657b94d9d72f339587721d2b02d9
AndrinaVincenz <56075988+AndrinaVincenz@users.noreply.github.com>
2019-10-14 17:59:38 +0200

    getpossibleDestination Rook

--- Soko2019/src/assignment2/Rook.java
+++ Soko2019/src/assignment2/Rook.java
@@ -15,4 +15,35 @@ public class Rook extends Piece {
     public String getName() {
     	return this.name;
     }
+ // Possible moves from King in a Boolean file with Size board, true/false
+ 	@Override
+ 	public boolean[][] getpossibleDestination(Board b) {
+ 		boolean[][] possibleM = new boolean[8][8];
+ 		for (int i = 0; i < possibleM.length; ++i) {
+ 			for (int j = 0; j < possibleM[i].length; ++j) {
+ 				possibleM[i][j] = false;
+ 			}
+ 		}
+ 		possibleM[currentfield.getX()][currentfield.getY()] = true;
+ 
+
+ 		for (int i = 0; i < 8; i++) {
+ 			// check if someone is on it!!!!
+ 			if (b.isValid(currentfield.getX(), i)) {
+ 				possibleM[currentfield.getX()][i] = true;
+ 			} 
+ 			if (b.isValid(i, currentfield.getY())) {
+ 				possibleM[i][currentfield.getY()] = true;
+ 		}
+ 		
+ 		}
+ 		for (int i = 0; i < possibleM.length; ++i) {
+ 			for (int j = 0; j < possibleM[i].length; ++j) {
+ 				System.out.print(possibleM[i][j]);
+ 			}
+ 			System.out.print("\n");
+ 		}
+
+ 		return possibleM;
+ 	}	
 }
