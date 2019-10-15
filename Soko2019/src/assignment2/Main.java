3a89803cbaf041b449a0279b3d7a48b075636a9b
Raphael <Koch@dhcp-wlan-uzh-10-12-157-162.uzh.ch>
2019-10-15 18:21:01 +0200

    converter added

--- Soko2019/src/assignment2/Main.java
+++ Soko2019/src/assignment2/Main.java
@@ -84,6 +84,25 @@ public class Main {
 		} else {
 			return false;
 		}
-	}			
+	}
+	
+	static int[] converted(String input){
+		int[] result = new int[2];
+		String[] letters = { "a", "b", "c", "d", "e", "f", "g", "h" };
+		String[] numbers = { "8", "7", "6", "5", "4", "3", "2", "1" };
+		int x = -1;
+		int y = -1;
+		for (int i = 0; i < letters.length; i++) {
+			if (input.substring(0, 1) == letters[i]) {
+				x = i;
+			}
+			if (input.substring(1, 2) == numbers[i]) {
+				y = i;
+			}
+		}
+		result[0] = x;
+		result[1] = y;
+		return result;
+	}
 
 }
