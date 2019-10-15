c4f450c325b9b8df282ccdf53cdda77b7966cfb1
AndrinaVincenz <56075988+AndrinaVincenz@users.noreply.github.com>
2019-10-15 15:03:35 +0200

    Bishop possdest method

--- Soko2019/src/assignment2/Bishop.java
+++ Soko2019/src/assignment2/Bishop.java
@@ -15,5 +15,55 @@ public class Bishop extends Piece {
     public String getName() {
     	return this.name;
     }
+    @Override
+ 	public boolean[][] getpossibleDestination(Board b) {
+ 		boolean[][] possibleM = new boolean[8][8];
+ 		for (int i = 0; i < possibleM.length; ++i) {
+ 			for (int j = 0; j < possibleM[i].length; ++j) {
+ 				possibleM[i][j] = false;
+ 			}
+ 		}
+
+ 		for (int i = 1; i <= 8; i++) {
+ 			
+ 			if (b.isValid(currentfield.getX()+i,currentfield.getY()+i)) {
+ 				possibleM[currentfield.getX()+i][currentfield.getY()+i] = true;
+ 				if (b.getBox(currentfield.getX()+i,currentfield.getY()+i).getPiece()!= null && b.getBox(currentfield.getX()+i,currentfield.getY()+i).getPiece().isWhite() == this.isWhite()) {
+					possibleM[currentfield.getX()+i][currentfield.getY()+i] = false;
+					break;
+				}
+ 			} 
+ 			if (b.isValid(currentfield.getX()+i,currentfield.getY()-i)) {
+ 				possibleM[currentfield.getX()+i][currentfield.getY()-i] = true;
+ 				if (b.getBox(currentfield.getX()+i,currentfield.getY()-i).getPiece()!= null && b.getBox(currentfield.getX()+i,currentfield.getY()-i).getPiece().isWhite() == this.isWhite()) {
+					possibleM[currentfield.getX()+i][currentfield.getY()-i] = false;
+					break;
+				}
+ 			}
+ 			if (b.isValid(currentfield.getX()-i,currentfield.getY()+i)) {
+ 				possibleM[currentfield.getX()-i][currentfield.getY()+i] = true;
+ 				if (b.getBox(currentfield.getX()-i,currentfield.getY()+i).getPiece()!= null && b.getBox(currentfield.getX()-i,currentfield.getY()+i).getPiece().isWhite() == this.isWhite()) {
+					possibleM[currentfield.getX()-i][currentfield.getY()+i] = false;
+					break;
+				}
+ 			}
+ 			if (b.isValid(currentfield.getX()-i,currentfield.getY()-i)) {
+ 				possibleM[currentfield.getX()-i][currentfield.getY()-i] = true;
+ 				if (b.getBox(currentfield.getX()-i,currentfield.getY()-i).getPiece()!= null && b.getBox(currentfield.getX()-i,currentfield.getY()-i).getPiece().isWhite() == this.isWhite()) {
+					possibleM[currentfield.getX()-i][currentfield.getY()-i] = false;
+					break;
+				}
+ 			}
+ 		
+ 		}
+ 		for (int i = 0; i < possibleM.length; ++i) {
+ 			for (int j = 0; j < possibleM[i].length; ++j) {
+ 				System.out.print(possibleM[i][j]);
+ 			}
+ 			System.out.print("\n");
+ 		}
+ 		possibleM[currentfield.getX()][currentfield.getY()] = true;
+ 		return possibleM;
+ 	}	
 
 }
