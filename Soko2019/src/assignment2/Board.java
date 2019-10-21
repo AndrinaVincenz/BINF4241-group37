59624e0c176bd0aa5a9cfe7b6779a4057e82196e
AndrinaVincenz <56075988+AndrinaVincenz@users.noreply.github.com>
2019-10-21 18:10:46 +0200

    Anpassung GetBoard()

--- Soko2019/src/assignment2/Board.java
+++ Soko2019/src/assignment2/Board.java
@@ -69,7 +69,7 @@ package assignment2;
 		public void getBoard(){
 			for (int i = 0; i < 8; i++) { 
 				for (int j = 0; j < 8; j++) { 
-					if (boxes[i][j].getPiece() != null) {
+					if (boxes[i][j].getPiece() != null && boxes[i][j].getPiece().isKilled() == false) {
 						if ((boxes[i][j].getPiece().isWhite())) {
 							String c = "W";
 							System.out.print("[" + c + boxes[i][j].getPiece().getName() + "]");
