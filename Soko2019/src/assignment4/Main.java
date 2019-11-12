f3bba163028bff8c63fa24636f2d5d1f37f46700
Reffx <ripsli@hotmail.com>
2019-11-12 21:40:47 +0100

    Some Workflow added

--- Soko2019/src/assignment4/Main.java
+++ Soko2019/src/assignment4/Main.java
@@ -1,8 +1,31 @@
 package assignment4;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
 	
 	public static void main(String args[]){
+		
+		Scanner scanner = new Scanner(System.in);
+		ElectronicDevice pickedDevice = null;
+		ArrayList<String> devices = new ArrayList<String>();
+		devices.add("Oven");
+		devices.add("Microwave");
+		
+		while (pickedDevice == null){
+			System.out.println("Choose one of the following devices:");
+			for(String device : devices) {
+	            System.out.println(device);
+	        }
+			String device = scanner.nextLine(); 
+			if (devices.contains(device)){
+				System.out.println(device);
+			} else {
+				System.out.println("This device doesn't exist!");
+			}
+		}
+		
 		
 		ElectronicDevice newOven = new Oven();
 		
@@ -19,7 +42,6 @@ public class Main {
 		smartphone  = new Smartphone(offCommand);
 		
 		smartphone.press();
-		
 		
 	}
 }
